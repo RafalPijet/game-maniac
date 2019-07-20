98b536072742cfbe691d48e356aedb2dfb47db50
Rafal Pijet <stronglopez@wp.pl>
2019-07-20 16:16:10 +0200

    Add basket counter

--- src/presentational/Header/Header.js
+++ src/presentational/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
+import {connect} from "react-redux";
 import BasketIcon from "../../data/icons/shopping-basket-solid.svg";
 import Logo from "../../data/icons/maniac-logo.png";
 import "./Header.css";
@@ -13,10 +14,19 @@ const Header = props => {
                 <NavLink className="item" exact to="/faq" activeClassName="active">FAQ</NavLink>
                 <NavLink className="item" exact to="/regulamin" activeClassName="active">REGULAMIN</NavLink>
                 <NavLink className="item" exact to="/kontakt" activeClassName="active">KONTAKT</NavLink>
-                <Link className="basket" to="/basket"><img className="icon" src={BasketIcon} alt="basket"/></Link>
+                <div className="header-basket">
+                    <Link className="basket" to="/basket"><img className="icon" src={BasketIcon} alt="basket"/></Link>
+                    <p className="basket-counter">{props.basketGames.length}</p>
+                </div>
             </nav>
         </div>
     )
 };
 
-export default Header;
+const MapStateToProps = store => {
+    return {
+        basketGames: store.basketReducer
+    }
+};
+
+export default connect(MapStateToProps)(Header);
